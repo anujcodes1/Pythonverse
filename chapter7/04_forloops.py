3be62eb581d39d14cce6d5a6376608f9ecb9898e
Anuj Mishra <anuj444mishra@gmail.com>
2026-06-11 11:42:10 +0530

    for_loops

---
+++ chapter7/04_forloops.py
@@ -0,0 +1,16 @@
+# for loops rane(start,stop,step)
+
+# l = ["anuj", 76 , "Mishra", "july" , "Cat" , 890]
+
+# for i in l:
+#     print(i)
+    
+    
+# t= (7, 45,67,34)
+# for i in t:
+#     print(i)
+    
+    
+s = "Harry"
+for i in s:
+    print(i)
