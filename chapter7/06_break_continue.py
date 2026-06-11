14b46087bbd93c99837eeea6b2d67d11c56a60d7
Anuj Mishra <anuj444mishra@gmail.com>
2026-06-11 12:24:11 +0530

    break_continue

---
+++ chapter7/06_break_continue.py
@@ -0,0 +1,12 @@
+# for i in range(100):
+#     if i==44:
+#         break #exit the loop right now
+#     print(i)
+    
+  
+  
+for i in range(100):
+    if i==44:
+        continue    #skips this iteration  
+    print(i)  
+    
