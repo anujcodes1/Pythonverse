f4baa326cc135426d06d0c6ae818093e8a4d552e
Anuj Mishra <anuj444mishra@gmail.com>
2026-06-16 12:04:34 +0530

    quickquiz

---
+++ chapter8/02_quickquiz.py
@@ -0,0 +1,9 @@
+#Quick Quiz:  Write a program to greet a user with “Good day” using functions.
+
+
+def greet():
+    a=input("Enter your message please: ")
+    
+    
+greet()
+print('hello sir, good morning how are your')
