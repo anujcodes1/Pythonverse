4f4503cafb980dfa4e6dc4a27f05f66b6f8b5e7f
Anuj Mishra <anuj444mishra@gmail.com>
2026-06-16 13:33:24 +0530

    problem2

---
+++ chapter8/practiceset8/problem_02.py
@@ -0,0 +1,11 @@
+#Write a python program using function to convert Celsius to Fahrenheit. 
+
+def temp():
+    
+    a = int(input("Enter the temperature in degree celsius: "))
+    far = a * 1.8 + 32
+    print("Fahrenheit temp is ", far )
+    
+temp()
+
+    
