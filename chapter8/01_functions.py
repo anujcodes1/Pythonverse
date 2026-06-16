0a3a3de2b29fb8e201b397de6b6960f54126a626
Anuj Mishra <anuj444mishra@gmail.com>
2026-06-16 12:01:17 +0530

    functions

---
+++ chapter8/01_functions.py
@@ -0,0 +1,42 @@
+# a = 14
+# b =34
+# c = 40
+
+# avg = ( a + b + c)/3
+
+# print(avg)
+
+
+# d = 70
+# e = 40
+# f = 20
+
+# avvg = (d + e + f)/3
+
+# print(avvg)
+
+# Function Definiton
+# def avg():
+#     a = int(input("Enter your first number: "))
+ 
+#     b = int(input("Enter your second number: "))
+#     c = int(input("Enter your third  number: "))
+    
+#     avg=(a+b+c)/3
+#     print(avg)
+    
+# avg()  #Function calling
+# print("Thank you")
+# avg()
+# print("Thank you")
+# avg()
+# print("Thank you")
+# avg()
+
+
+def greet():
+    a=input("Enter your message please: ")
+    
+    
+greet()
+print('hello sir, good morning how are your')
