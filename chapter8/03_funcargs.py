a38eb39ab16842a42d3ef98af3c0bcc4eff4ca48
Anuj Mishra <anuj444mishra@gmail.com>
2026-06-16 12:15:35 +0530

    function_with_arguements

---
+++ chapter8/03_funcargs.py
@@ -0,0 +1,9 @@
+#functions with arguments
+
+def goodDay(name , ending):
+    print("Good Day " + name)
+    print(ending)
+    
+goodDay("Anuj", "Thank you")
+goodDay("Rohan", "Thank you")
+goodDay("Akash", "Thank you")
