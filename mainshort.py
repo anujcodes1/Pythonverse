15951999c831b77b0eab44d20106ef4dd49fe402
Anuj Mishra <anuj444mishra@gmail.com>
2026-06-18 12:21:09 +0530

    mainshort

---
+++ mainshort.py
@@ -0,0 +1,24 @@
+
+import random
+computer = random.choice([-1, 0, 1])
+youstr = input("Enter your choice: ")
+youDict = {"s": 1, "w": -1 , "g": 0}
+
+reverseDict = {1: "Snake", -1:"Water", 0: "Gun"}
+
+you = youDict[youstr]
+
+# By now we have 2 numbers (variables), you and computer
+
+print(f"You choose {reverseDict[you]} Computer choose {reverseDict[computer]}")
+
+
+if(computer==you):
+    print("its draw!")
+    
+else:
+    
+    if((computer - you) == -1 or (computer - you) == 2):
+        print("You lose")
+    else:
+        print("You win")
