047fe3d1a2607a5c3e805d9b690b0e8cc20d848a
Anuj Mishra <anuj444mishra@gmail.com>
2026-06-20 16:21:17 +0530

    problem_03

--- chapter9/practiceset_09/problem_03.py
+++ chapter9/practiceset_09/problem_03.py
@@ -1,3 +1,6 @@
+#Write a program to generate multiplication tables from 2 to 20 and write it to the 
+# different files. Place these files in a folder for a 13 – year old.
+
 word = "Donkey"
 
 with open("chapter9/practiceset_09/file.txt", "r") as f:
