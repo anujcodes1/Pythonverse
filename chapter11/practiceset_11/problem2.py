5e866a4adf6e23d663a4e94644939bd6de42dc00
Anuj Mishra <anuj444mishra@gmail.com>
2026-07-10 22:35:49 +0530

    problem2

---
+++ chapter11/practiceset_11/problem2.py
@@ -0,0 +1,18 @@
+class Animals:
+    pass
+
+class Pets(Animals):
+    pass
+
+class Dog(Pets):
+    
+    def bark():
+        print("The dog is barking: bow bow")
+       
+    bark()  
+
+d = Dog()
+
+
+
+    
