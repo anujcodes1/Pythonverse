c179b74813d7959744e23ca40599dcffe171b4e7
Anuj Mishra <anuj444mishra@gmail.com>
2026-07-10 21:05:54 +0530

    classmethod

---
+++ chapter11/05_classmethod.py
@@ -0,0 +1,13 @@
+class Employee:
+    a = 99
+    
+    @classmethod
+    def show(cls):
+        print(f"The class attribute of a class a is {cls.a}")
+        
+        
+a = Employee()
+
+a = 45
+
+print(a)
