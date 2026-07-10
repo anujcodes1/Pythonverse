5465659ad74f14c6b1247b2096446827582aa5df
Anuj Mishra <anuj444mishra@gmail.com>
2026-07-10 20:59:39 +0530

    super.py

---
+++ chapter11/04_super.py
@@ -0,0 +1,24 @@
+class Employee:
+    def __init__(self):
+        print("Constructor of Employee")
+    a =1
+    
+class programmer(Employee):
+     def __init__(self):
+        print("Constructor of programmer")
+     b =2
+    
+class Manager(programmer):
+     def __init__(self):
+         super().__init__()
+         print("Constructor of manager")
+     c =3
+    
+# o = Employee()
+# print(o.a)
+
+# o = programmer()
+# print(o.a , o.b)
+
+o = Manager()
+print(o.a , o.b , o.c)
